5b336e535c90705818442275f43817e01617ce89
Vyctoria <vyctoriak@gmail.com>
2020-10-01 01:17:53 -0300

    aula de métodos com retorno no metodo 'saca'

--- Conta.java
+++ Conta.java
@@ -6,6 +6,18 @@ class Conta {
 
     public void deposita(double valor) {
         this.saldo = this.saldo + valor;
+        System.out.println("Você adicionou " + valor + " na sua conta!");
+    }
+
+    public boolean saca(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            System.out.println("Saque de " + saldo + " realizado com sucesso!");
+            return true;
+        } else {
+            System.out.println("Você não tem saldo suficiente para esse saque!");
+            return false;
+        }
     }
 
 }
--- TestaMetodo.java
+++ TestaMetodo.java
@@ -1,18 +1,31 @@
 public class TestaMetodo {
 
     public static void main(String[] args) {
+        /*
+        Primeira conta bancária
+         */
+
         Conta contaDaVyc = new Conta();
 
         contaDaVyc.saldo = 100;
         contaDaVyc.deposita(50);
 
+        System.out.println("Conta da vyc: " + contaDaVyc.saldo);
+        boolean conseguiuRetirar = contaDaVyc.saca(300);
         System.out.println(contaDaVyc.saldo);
+        System.out.println(conseguiuRetirar);
+
+        /*
+        Segunda conta bancária
+         */
 
         Conta contaDaLia = new Conta();
         contaDaLia.saldo = 50;
         contaDaLia.deposita(50);
-
+        System.out.println("Conta da Lia: " + contaDaLia.saldo);
+        conseguiuRetirar = contaDaLia.saca(50);
         System.out.println(contaDaLia.saldo);
+        System.out.println(conseguiuRetirar);
 
     }
 
